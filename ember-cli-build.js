39d4645906655e0a5e06c46ef56b6e79952889b0
charlesagyemang <micnkru@gmail.com>
2018-05-14 16:13:10 +0000

    Set all up for a default landing page

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,10 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  app.import('vendor/assets/css/creative.css');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
